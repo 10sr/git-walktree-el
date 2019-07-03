91ec992b4617d4cf8c3b37966720344b4130fc12
10sr <8.slashes@gmail.com>
2019-07-03 14:36:58 +0900

    Add comment

--- git-walktree.el
+++ git-walktree.el
@@ -233,6 +233,7 @@ It also copy text overlays."
   "Command used show current commit object in tree buffer.")
 
 ;; TODO: Fix when BUFFER is given
+;; 治った気がする？
 (defun git-walktree--load-treeish (commitish path treeish &optional buffer)
   "Open git tree buffer of COMMITISH:PATH.
 
